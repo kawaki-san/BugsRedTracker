bd09eef7bfba8b8a7cd93393c254a72a14c16ecd
Kawaki-san <rtkayy123@gmail.com>
2021-01-27 15:51:37 +0200

    adds h2 dependency

--- app/src/main/java/com/rtkay/controller/Controller.java
+++ app/src/main/java/com/rtkay/controller/Controller.java
@@ -3,14 +3,18 @@ package com.rtkay.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 public class Controller implements Initializable {
+
+    @FXML
+    private Button btnSignIn;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // TODO Auto-generated method stub
-        System.out.println("Hello");
 
     }
 
